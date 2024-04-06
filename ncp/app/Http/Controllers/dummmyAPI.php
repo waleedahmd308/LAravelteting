9c6eb88774bfb491636089de685eec2a1b6aae8f
waleedahmd308 <wa353457@gmail.com>
2024-04-06 09:12:25 +0500

    New:Added new project for live server

---
+++ ncp/app/Http/Controllers/dummmyAPI.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dummmyAPI extends Controller
+{
+    //
+  function getData() {
+    return [
+        "name"=>"waleed"
+    ];
+    
+  }
+}
---
+++ ncp/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        // $this->call([
+        //     UserSeeder::class,
+        //     Po
+        // ])
+
+        DB::table('books')->insert([
+            'name'=>Str::random(10),
+            'author' => Str::random(10),
+            'publish_date'=>Hash::make('publish_date'),
+        ]);
+    }
+}
